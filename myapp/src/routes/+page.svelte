22e887c2f03ba28768aeea7006930a49e1c3403b
mbelmontes7 <you@example.com>
2023-02-08 16:33:12 -0600

    Arranged the svgs

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,9 +1,14 @@
-
-<script>
+<script lang="ts">
 
 </script>
-<main>
-	<h1 class="text-4xl font-bold text-center text-yellow-800 md:text-8xl"
-	>Learn Your Multiplication tables </h1>
-</main>
 
+<!-- <img src={Bkcolor} alt="Bkcolor" /> -->
+
+<!-- <div class="background"></div> -->
+
+<main>
+	<h1 class="text-4xl font-bold text-center text-yellow-800 md:text-8xl">
+		Learn Your Multiplication tables
+	</h1>
+	
+</main>
