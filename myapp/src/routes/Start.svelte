0eed54cd720043d1edad6568ef7c60cb20a7fca1
mbelmontes7 <you@example.com>
2023-02-15 18:19:29 -0600

    The lattest version of svelte -use a + sign to render the pages

--- myapp/src/routes/Start.svelte
+++
@@ -1,5 +0,0 @@
-<div class="Start">
-<h2> Abod
-</h2>
-
-</div>
---
+++ myapp/src/routes/start/+page.svelte
@@ -0,0 +1,5 @@
+<div class="Start">
+<h2> Acd</h2>
+<a href="/+page"><!-- In this section you made a svg and made it clickable  -->
+
+</div>
